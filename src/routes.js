21acd7130ae5cb6bdea5d7ef630d60f14422a246
codingscode <heronnestg@hotmail.com>
2023-03-28 21:17:16 -0300

    parte 2

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 
+const { Router } = require('express')
 
 
+const routes = Router()
 
+routes.get('/health', (req, res) => {
 
+   return res.status(200).json({message: 'Servidor está on...'})
+})
 
-
-
-
+module.exports = routes
 
